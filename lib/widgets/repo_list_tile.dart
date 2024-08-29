68390eda0d9cad62765608df43adb9585fd7db35
kukurio218 <108991390+kukurio218@users.noreply.github.com>
2024-08-29 23:27:53 +0900

    UIの改善

--- lib/widgets/repo_list_tile.dart
+++ lib/widgets/repo_list_tile.dart
@@ -9,18 +9,44 @@ class RepoListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(repo.name),
-      subtitle: Text(repo.language),
-      leading: Image.network(repo.ownerAvatarUrl),
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => RepoDetailPage(repo),
+    return Card(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(10.0),
+        title: Text(
+          repo.name,
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+            color: Colors.black87,
           ),
-        );
-      },
+          overflow: TextOverflow.ellipsis,
+        ),
+        subtitle: Text(
+          repo.language ?? 'Unknown',
+          style: TextStyle(
+            fontSize: 14,
+            color: Colors.black54,
+          ),
+        ),
+        leading: ClipRRect(
+          borderRadius: BorderRadius.circular(25.0),
+          child: Image.network(repo.ownerAvatarUrl, width: 50, height: 50),
+        ),
+        trailing: Icon(Icons.arrow_forward, color: Colors.black),
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => RepoDetailPage(repo),
+            ),
+          );
+        },
+      ),
+      color: Colors.white,
     );
   }
 }
